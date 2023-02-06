2442822f60016671d0982aff66fa849dd542109a
Juan Sebastian Ortiz Castro <sebastianortizcastro4@gmail.com>
2023-02-05 21:52:29 -0600

    first commit

---
+++ resources/src/App.vue
@@ -0,0 +1,17 @@
+<template>
+    <h1>{{ titulo }}</h1>
+</template>
+
+<script>
+export default {
+    data(){
+        return{
+            titulo:'Prueba Leonali Laravel + vite +vue 3'
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
